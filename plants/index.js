0c21458e2e681e4e873c305f23e1b0da17bbdd42
TetianaDorosh <doroshtetiana77@gmail.com>
2023-03-05 18:03:11 +0100

    remove assessment in the beggining

--- plants/index.js
+++ plants/index.js
@@ -1,30 +1,3 @@
-// let mark = 100;
-// const assessment = {
-//   "Вёрстка валидная": 10,
-//   "Вёрстка семантическая": 20,
-//   "Вёрстка соответствует макету": 38,
-//   "Требования к css": 12,
-//   "Интерактивность, реализуемая через css": 20,
-
-//   result: function () {
-//     const resultArr = Object.values(this).filter(
-//       (item) => typeof item === "number"
-//     );
-//     const result = resultArr.reduce((prev, item) => {
-//       return (prev = prev + item);
-//     }, 0);
-//     return result;
-//   },
-// };
-
-// for (item in assessment) {
-//   typeof assessment[item] === "number"
-//     ? console.log(`${item} - ${assessment[item]}`)
-//     : "";
-// }
-
-// console.log(`My self-assessment is: ${assessment.result()} points`);
-
 //Код для ссылки в меню. Активная ссылка выделяется красным цветом.
 
 const link = document.querySelector(".header__nav-list");
